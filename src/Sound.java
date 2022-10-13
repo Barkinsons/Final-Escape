00429fa87b2dc3a954312d80295e22c677a57f13
Jared Sevilla <jaredgsevilla@gmail.com>
2022-10-13 16:26:50 -0700

    sound class creation

---
+++ src/Sound.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import javax.sound.sampled.*;
+
+public class Sound {
+    Clip clip;
+    File[] files = new File[30];
+
+    public Sound() {
+        files[0] = new File("res/audio/bg.wav");
+        files[1] = new File("res/audio/button_forward.wav");
+    }
+
+    public void set_file(int i) {
+
+        try {
+
+            AudioInputStream ais = AudioSystem.getAudioInputStream(files[i]);
+            clip = AudioSystem.getClip();
+            clip.open(ais);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void play() {
+        clip.start();
+    }
+
+    public void loop() {
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void stop() {
+        clip.stop();
+    }
+
+}
